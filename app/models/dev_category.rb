90d5c710f6065acb72539d0994c7e4f0cb5db082
Ann <59127075+kotogavr@users.noreply.github.com>
2024-02-11 10:16:26 +0000

    init

---
+++ app/models/dev_category.rb
@@ -0,0 +1,2 @@
+class DevCategory < ApplicationRecord
+end
---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  get  'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  get  'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  resources :sessions, only: %i[index show destroy]
+  resource  :password, only: %i[edit update]
+  namespace :identity do
+    resource :email,              only: %i[edit update]
+    resource :email_verification, only: %i[show create]
+    resource :password_reset,     only: %i[new edit create update]
+  end
+  get  '/auth/failure',            to: 'sessions/omniauth#failure'
+  get  '/auth/:provider/callback', to: 'sessions/omniauth#create'
+  post '/auth/:provider/callback', to: 'sessions/omniauth#create'
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
